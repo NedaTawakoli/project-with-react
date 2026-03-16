211c09eab8a34e46928523d24468d707ab6f7376
NedaTawakoli <tawakolineda@gmail.com>
2026-03-16 23:59:30 +0430

    component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Navbar from "./Nav";
+
+function App(){
+  return <div>
+    <h1>Afghanistan</h1>
+{/* <Navbar/> */}
+    <Header/>
+  </div>
+}
+export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+        <>
+        <nav>
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Contact</h1>
+            <h1>Project</h1>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
